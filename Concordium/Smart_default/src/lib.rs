7218b7a0d3722d12494529ef46570562f3c23e75
Fomalhaut <inso32@gmail.com>
2023-11-20 05:58:45 +0500

    Update lib.rs

--- Concordium/Smart_default/src/lib.rs
+++ Concordium/Smart_default/src/lib.rs
@@ -1,1 +1,58 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 
+//! # A Concordium V1 smart contract
+use concordium_std::*;
+use core::fmt::Debug;
+
+/// Your smart contract state.
+#[derive(Serialize, SchemaType)]
+pub struct State {
+    // Your state
+}
+
+/// Your smart contract errors.
+#[derive(Debug, PartialEq, Eq, Reject, Serialize, SchemaType)]
+pub enum Error {
+    /// Failed parsing the parameter.
+    #[from(ParseError)]
+    ParseParams,
+    /// Your error
+    YourError,
+}
+
+/// Init function that creates a new smart contract.
+#[init(contract = "three_signs")]
+fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State> {
+    // Your code
+
+    Ok(State {})
+}
+
+pub type MyInputType = bool;
+
+/// Receive function. The input parameter is the boolean variable `throw_error`.
+///  If `throw_error == true`, the receive function will throw a custom error.
+///  If `throw_error == false`, the receive function executes successfully.
+#[receive(
+    contract = "three_signs",
+    name = "receive",
+    parameter = "MyInputType",
+    error = "Error",
+    mutable
+)]
+fn receive(ctx: &ReceiveContext, _host: &mut Host<State>) -> Result<(), Error> {
+    // Your code
+
+    let throw_error = ctx.parameter_cursor().get()?; // Returns Error::ParseError on failure
+    if throw_error {
+        Err(Error::YourError)
+    } else {
+        Ok(())
+    }
+}
+
+/// View function that returns the content of the state.
+#[receive(contract = "three_signs", name = "view", return_value = "State")]
+fn view<'b>(_ctx: &ReceiveContext, host: &'b Host<State>) -> ReceiveResult<&'b State> {
+    Ok(host.state())
+}
